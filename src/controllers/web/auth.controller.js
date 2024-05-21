5a398dd1f56906e8ce02870ecf373e310ea254cc
Daniel Bolaños <danielbolanos909@gmail.com>
2024-05-20 22:19:26 -0600

    resolve login

--- src/controllers/web/auth.controller.js
+++ src/controllers/web/auth.controller.js
@@ -7,42 +7,47 @@ const { SECRET_TOKEN } = process.env;
 
 const serviceAuth = new authService();
 
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   const data = req.body;
-  const result = await serviceAuth.login(data);
-  if (result.length == 0) {
-    return res.send({
-      Auth: false,
-      message: "Usuario o Contraseña Incorrectos",
-    });
-  } else {
-    const validation = await bcrypt.compare(data.password, result[0].password);
-    if (!validation) {
+  try {
+    const result = await serviceAuth.login(data);
+    if (result.length == 0) {
       return res.send({
         Auth: false,
         message: "Usuario o Contraseña Incorrectos",
       });
     } else {
-      const token = jwt.sign(
-        {
-          name: result[0].NOMBRES + result[0].APELLIDOS,
-          ID: result[0].idUsuario,
-        },
-        SECRET_TOKEN,
-        { expiresIn: 84000 }
+      const validation = await bcrypt.compare(
+        data.password,
+        result[0].CONTRASEÑA
       );
-      res.cookie("Auth", token, {
-        maxAge: 3600000, // Duración de la cookie en milisegundos (1 hora en este caso)
-        httpOnly: true, // Hace que la cookie sea accesible solo desde el servidor
-        secure: true,
-      });
-      res.send({
-        Auth: true,
-        message: `BIENVENIDO DE NUEVO ${
-          result[0].NOMBRES + result[0].APELLIDOS
-        }`,
-      });
+      if (!validation) {
+        return res.send({
+          Auth: false,
+          message: "Usuario o Contraseña Incorrectos",
+        });
+      } else {
+        const token = jwt.sign(
+          {
+            name: `${result[0].NOMBRES} ${result[0].APELLIDOS}`,
+            ID: result[0].idUsuario,
+          },
+          SECRET_TOKEN,
+          { expiresIn: 84000 }
+        );
+        res.cookie("Auth", token, {
+          maxAge: 3600000, // Duración de la cookie en milisegundos (1 hora en este caso)
+          httpOnly: true, // Hace que la cookie sea accesible solo desde el servidor
+          secure: true,
+        });
+        res.send({
+          Auth: true,
+          message: `BIENVENIDO DE NUEVO ${result[0].NOMBRES} ${result[0].APELLIDOS}`,
+        });
+      }
     }
+  } catch (error) {
+    next(error);
   }
 };
 
